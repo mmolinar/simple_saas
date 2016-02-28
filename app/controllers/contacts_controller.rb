995ecccb9ad7802f902fa6e48c52dc1feec554db
mariamolinaryarza@gmail.com <mariamolinaryarza@gmail.com>
2016-02-28 14:32:06 +0000

    Add flash notice

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,19 @@ class ContactsController < ApplicationController
   end
   
   def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      flash[:success] = 'Message envoyé. Nous vous contacterons dans le meilleur délai.'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Message non envoyé.'
+      redirect_to new_contact_path
+    end
   end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
